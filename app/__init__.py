521d59145fb14621e6adef27f3974f7652da5cbb
Varun S Lingaraju <warunsl@gmail.com>
2013-11-15 17:17:58 -0800

    Fied an indentation bug

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
