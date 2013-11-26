9cf0754a65bd947528d523e4d6ddfcb45d9c3c35
Varun S Lingaraju <warunsl@gmail.com>
2013-11-25 22:12:50 -0800

    Added calais related routines

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,40 +1,60 @@
 import pymongo
+from calais import Calais
+
+API_KEY = u'xy6hh7fgx7qqywt7mzgkqarn'
+calais = Calais(API_KEY, submitter='artists-info')
+
+
+def get_entities_from_calais(artist):
+    calais_result = calais.analyze(artist)
+    return calais_result.entities
+
+
+def get_topics_from_calais(artist):
+    calais_result = calais.analyze(artist)
+    return calais_result.topics
+
+
+def get_relations_from_calais(artist):
+    calais_result = calais.analyze(artist)
+    return calais_result.relations
+
 
 def get_popular_search_keywords_from_db():
-	pass
+    pass
 
 
 def get_all_search_keywords_from_db():
-	pass
+    pass
 
 
 def set_popular_search_keywords_to_db():
-	pass
+    pass
 
 
 def set_all_search_keywords_to_db():
-	pass
+    pass
 
 
 def compute_popular_searches(new_keyword):
-	current_top_n = get_popular_search_keywords_from_db()
-	current_all_searches = get_all_search_keywords_from_db()
-	new_keyword_count = 0
+    current_top_n = get_popular_search_keywords_from_db()
+    current_all_searches = get_all_search_keywords_from_db()
+    new_keyword_count = 0
 
-	try:
-		current_all_searches[new_keyword] += 1
-	except KeyError:
-		current_all_searches[new_keyword] = 1
-	new_keyword_count = current_all_searches[new_keyword]
+    try:
+        current_all_searches[new_keyword] += 1
+    except KeyError:
+        current_all_searches[new_keyword] = 1
+    new_keyword_count = current_all_searches[new_keyword]
 
-	current_top_n_values = current_top_n.values()
+    current_top_n_values = current_top_n.values()
 
+    if new_keyword_count > min(current_top_n_values):
+        current_top_n_values.append(new_keyword_count)
+        current_top_n_values = current_top_n_values[:5]
+        current_top_n[new_keyword] = new_keyword_count
+        current_top_n = {k: v for k, v in current_top_n.items()
+                         if v in current_top_n_values}
 
-	if new_keyword_count > min(current_top_n_values):
-		current_top_n_values.append(new_keyword_count)
-		current_top_n_values = current_top_n_values[:5]
-		current_top_n[new_keyword] = new_keyword_count
-		current_top_n = {k:v for k,v in current_top_n.items() if v in current_top_n_values}
-
-	set_popular_search_keywords_to_db()
-	set_all_search_keywords_to_db()
+    set_popular_search_keywords_to_db()
+    set_all_search_keywords_to_db()
