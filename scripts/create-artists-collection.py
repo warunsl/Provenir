8bd6da5d4dcbb2310b277f64eda5b35b95f5328e
Varun S Lingaraju <warunsl@gmail.com>
2013-11-27 14:54:56 -0800

    Merges artists from reconciled data output of Open Refine

--- scripts/create-artists-collection.py
+++ scripts/create-artists-collection.py
@@ -1,48 +1,112 @@
 import os
+import sys
+import csv
 import json
+import pymongo
+
+'''
+    Create JSON for the artist
+
+    artist = {
+        "name" : "",
+        "url" : "",
+        "country" : "",
+        "short_description" : "",
+        "long_description" : "",
+        "era" : "",
+        "influencer_of" : [
+            1,
+            2,
+            3
+        ],
+        "influenced_by" : [
+            1,
+            2,
+            3
+        ],
+    }
+
+'''
+
+artists = {}
+
+scripts_dir = os.getcwd()
+base_dir = os.path.dirname(scripts_dir)
+data_dir = os.path.abspath(os.path.join(base_dir, 'data'))
+refine_dir = os.path.abspath(os.path.join(data_dir, 'refine'))
+
+getty_reconciled_artists = os.path.abspath(os.path.join(refine_dir,
+                                           'getty-artists-dbpedia.csv'))
+nga_reconciled_artists = os.path.abspath(os.path.join(refine_dir,
+                                         'nga-artists-dbpedia.csv'))
 
 
-def main():
+def get_artist_details():
     '''
-        TO-DO : Make sure both the input files are encoded correctly
+        For the entries that were re-conciled successfully with dbpedia
+        we fetch the artist details from dbpedia by doing a sparql query
+
+        For the rest:
+        1. If it was a nga artist, we get the details from nga json
+        2. Else, blanks
     '''
-    artists = set()
-    with open('nga-artists', 'rb') as ngafile:
-        for line in ngafile:
-            artists.add(line.decode('latin-1'))
+    global artists
+    pass
+
+
+def process_files():
+    '''
+        Creates artist dictionary with name as the Key
+        name = (name, url)
+    '''
+
+    global artists
+
+    if not os.path.exists(getty_reconciled_artists) and not os.path.exists(nga_reconciled_artists):
+        print "No input files"
+        return False
+
+    with open(nga_reconciled_artists, 'rb') as ngafile:
+        csv_reader = csv.reader(ngafile)
+        for row in csv_reader:
+            try:
+                # If we hit a duplicate artist, select the one with
+                # url which is not foaf:Person
+                artists[row[0]]
+                url = artists[row[0]][1]
+                if "Person" not in url:
+                    artists[row[0]] = (row[0], url)
+                    # print "Specific url found: ", url
+                else:
+                    # print "Duplicate artist found. Skipping: ", row[0]
+                    pass
+            except KeyError:
+                artists[row[0]] = (row[0], row[3])
     ngafile.close()
 
-    with open('getty-artists', 'rb') as gettyfile:
-        for line in gettyfile:
-            artists.add(line.decode('latin-1'))
+    with open(getty_reconciled_artists, 'rb') as gettyfile:
+        csv_reader = csv.reader(gettyfile)
+        for row in csv_reader:
+            try:
+                # If we hit a duplicate artist, select the one with
+                # url which is not foaf:Person
+                artists[row[0]]
+                url = artists[row[0]][1]
+                if "Person" not in url:
+                    artists[row[0]] = (row[0], url)
+                    # print "Specific url found: ", url
+                else:
+                    # print "Duplicate artist found. Skipping: ", row[0]
+                    pass
+            except KeyError:
+                artists[row[0]] = (row[0], row[2])
     gettyfile.close()
 
-    '''
-        Create JSON for the artist
-
-        artist = {
-            "name" : "",
-            "country" : "",
-            "short_description" : "",
-            "long_description" : "",
-            "era" : "",
-            "influencer_of" : [
-                1,
-                2,
-                3
-            ],
-            "influenced_by" : [
-                1,
-                2,
-                3
-            ],
-        }
-
-    '''
+    # prepare the json to be inserted into the mongo collection
     artist_json = []
-    for artist in artists:
+    for k, v in artists.items():
         artist_dict = {}
-        artist_dict["name"] = unicode(artist.strip())
+        artist_dict["name"] = artists[k][0]
         artist_dict["country"] = ""
         artist_dict["short_description"] = ""
         artist_dict["long_description"] = ""
@@ -54,6 +118,12 @@ def main():
     with open('mongo_artists.json', 'w') as outfile:
         outfile.write(json.dumps(artist_json))
     outfile.close()
+
+
+def main():
+    if not process_files():
+        sys.exit(1)
+    get_artist_details()
 
 
 if __name__ == '__main__':
