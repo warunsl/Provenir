d9e650f9f8f7dd771d84dfbf9d324ece04f1b7d6
Varun S Lingaraju <warunsl@gmail.com>
2013-11-28 00:45:04 -0800

    Fixed encoding issues with BeautifulSoup:

--- scripts/get-artists-nga.py
+++ scripts/get-artists-nga.py
@@ -1,11 +1,12 @@
 import json
 import os
 from pprint import pprint
+from BeautifulSoup import BeautifulSoup
 
 artists_set = set()
 
 
-# Converts the unicode keys in the dictionary to string
+# Converts the encoded keys in the dictionary to string
 def convert(input):
     if isinstance(input, dict):
         return {convert(key): convert(value) for
@@ -13,7 +14,7 @@ def convert(input):
     elif isinstance(input, list):
         return [convert(element) for element in input]
     elif isinstance(input, unicode):
-        return input.encode('utf-8')
+        return input.encode('latin-1')
     else:
         return input
 
@@ -23,7 +24,7 @@ def process_artists(artists_list):
 
     for artist in artists_list:
         name = artist[u'name']
-        # print name
+        name = unicode(BeautifulSoup(name, convertEntities=BeautifulSoup.HTML_ENTITIES))
         fullname = name.strip().split(',')
         if len(fullname) == 2:
             artists_set.add(fullname[1].strip() + ' ' + fullname[0].strip())
@@ -41,11 +42,12 @@ def main():
                 artists_list = entry["artists"]
                 process_artists(artists_list)
             except Exception, TypeError:
-                print "Not a valid json record. Slipping ..."
+                print "Not a valid json record. Skipping ..."
     jsonfile.close()
 
     with open('nga-artists', 'w') as outfile:
         for artist in artists_set:
+            artist = convert(artist)
             outfile.write(artist)
             outfile.write('\n')
     outfile.close()
