f7e01c63c9764524e330cda50c9f9c03c3d4b85a
Varun S Lingaraju <warunsl@gmail.com>
2013-12-06 11:21:46 -0800

    Implementation to list arts for artists from NGA

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 import bson
 import pymongo
+from pprint import pprint
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from BeautifulSoup import BeautifulSoup
 
 connection = MongoClient()
 db = connection.provenir
+gldb = connection.gladondb
 artist_collection = db.artist
-art_collection = db.art
+artist_to_art_collection = gldb.artisttoart
+art_data_collection = gldb.artdata
 
 app = Flask(__name__)
 
@@ -30,6 +34,24 @@ def artist(artistid=None):
     try:
         artist_object = artist_collection.find_one({'_id':bson.ObjectId(oid=str(artistid))})
         if artist_object:
+            if artist_object['source'] == 'nga':
+                # first = artist_object['nga-data']['url'].split('.html')[0]
+                # nga_id = first[first.index('.') + 1:]
+                nga_artist_url = artist_object['nga-data']['url']
+                artist_to_art_record = artist_to_art_collection.find_one({'artistURL':nga_artist_url})
+                if artist_to_art_record:
+                    art_ids = artist_to_art_record['arts']
+                    arts = []
+                    if len(art_ids) > 0:
+                        artist_object['art_ids'] = art_ids
+                        for art_id in art_ids:
+                            art_data_record = art_data_collection.find_one({'id':art_id})
+                            if art_data_record:
+                                title = unicode(BeautifulSoup(art_data_record['title'], convertEntities=
+                                   BeautifulSoup.HTML_ENTITIES))
+                                arts.append({'id': art_id, 'title':title, 'image':art_data_record['imagepath']})
+                        artist_object['arts'] = arts
+            pprint(artist_object)
             return render_template('artist.html', artist=artist_object)
         else:
             return render_template('404.html')
