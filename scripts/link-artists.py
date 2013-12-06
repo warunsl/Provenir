761f14e141c3f7a47ce12e672f6522f2cc1a634c
Varun S Lingaraju <warunsl@gmail.com>
2013-12-06 14:28:28 -0800

    Code to remove duplicate reconciled artists

--- scripts/link-artists.py
+++ scripts/link-artists.py
@@ -12,6 +12,31 @@ db = connection.provenir
 collection = db.artist
 
 
+def fix_duplicate_dbpedia():
+    dbpedia_artists = []
+    dbpedia_artists_cursor = collection.find({'linked':'True'})
+    for record in dbpedia_artists_cursor:
+        dbpedia_artists.append(record['url'])
+    print "Before deduplication", len(dbpedia_artists)
+    # dbpedia_artists = list(set(dbpedia_artists))
+    # print "After deduplication", len(dbpedia_artists)
+    count = 0
+    for artist_url in dbpedia_artists:
+        cursor = collection.find({'linked':'True', 'url':artist_url})
+        for record in cursor:
+            if record['source'] == 'nga':
+                record_to_update = record
+                break
+            else:
+                record_to_update = record
+        # Remove all the artist records for this url
+        collection.remove({'linked':'True', 'url':artist_url})
+        # Add the one we saved
+        collection.insert(record_to_update)
+    dbpedia_artists_cursor = collection.find({'linked':'True'})
+    print "After deduplication", len(set(list(dbpedia_artists)))
+
+
 #db.test.update({"x": "y"}, {"$set": {"a": "c"}})
 def update_collection():
     dbpedia_regx = re.compile("/.*dbpedia.*/", re.IGNORECASE)
@@ -42,6 +67,7 @@ def get_common_linked_urls():
 def main():
     get_common_linked_urls()
     update_collection()
+    fix_duplicate_dbpedia()
 
 
 if __name__ == '__main__':
