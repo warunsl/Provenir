04ca2ebb1bfd7420516c748df6ba87db43a20ec2
Varun S Lingaraju <warunsl@gmail.com>
2013-12-06 10:09:23 -0800

    Artist fetch implementation

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
+import bson
+import pymongo
 from flask import Flask
 from flask import render_template
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+connection = MongoClient()
+db = connection.provenir
+artist_collection = db.artist
+art_collection = db.art
 
 app = Flask(__name__)
 
@@ -14,8 +23,16 @@ def about():
 
 @app.route('/art')
 def art():
-	return render_template('art.html')
+    return render_template('art.html')
 
-@app.route('/artist')
-def artist():
-	return render_template('artist.html')
+@app.route('/artist/<artistid>')
+def artist(artistid=None):
+    try:
+        artist_object = artist_collection.find_one({'_id':bson.ObjectId(oid=str(artistid))})
+        if artist_object:
+            return render_template('artist.html', artist=artist_object)
+        else:
+            return render_template('404.html')
+    except bson.errors.InvalidId, e:
+        return render_template('404.html')
+    
